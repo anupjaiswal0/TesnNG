b6fc345496c45ac46be49c20f2aac81b5d55ca14
rahul <rahul@localhost>
2025-02-06 18:18:10 +0530

    Reading the data from the Excel file and added the dependencies

---
+++ com.crm.MyAdvanceM5/src/test/java/Basic/OrangeHrm.java
@@ -0,0 +1,23 @@
+package Basic;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OrangeHrm {
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15000));
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+//		driver.findElement(By.partialLinkText("Log in")).click();
+		driver.findElement(By.xpath("//input[@name='username']")).sendKeys("Admin");
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[text()=' Login ']")).click();
+
+		Thread.sleep(3000);
+	}
+}
--- com.crm.MyAdvanceM5/src/test/java/TestNGExe/OrangeHRM.java
+++ com.crm.MyAdvanceM5/src/test/java/TestNGExe/OrangeHRM.java
@@ -10,20 +10,20 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-public class UsingDataProvider {
+public class OrangeHRM {
 	@DataProvider(name = "login")
 	public Object[][] sender() throws EncryptedDocumentException, IOException {
 //		System.out.println("I'm a data provider");
-		Reporter.log("I'm a data provider");
-		FileInputStream fis = new FileInputStream("/Users/rahul/Documents/DwsLogin.xlsx");
+//		Reporter.log("I'm a data provider");
+		Reporter.log("I'm a data provider", true); // to see the message on cosole
+		FileInputStream fis = new FileInputStream("/Users/rahul/Downloads/OrangHRM.xlsx");
 		Workbook wb = WorkbookFactory.create(fis);
-		Sheet sheet = wb.getSheet("Sheet1");
+		Sheet sheet = wb.getSheet("Sheet2");
 		int rows = sheet.getPhysicalNumberOfRows();
 		int colums = sheet.getRow(0).getPhysicalNumberOfCells();
 		Object[][] obj = new Object[rows][colums];
@@ -40,18 +40,18 @@ public class UsingDataProvider {
 	}
 
 	@Test(dataProvider = "login")
-	public void dws(String username, String password) throws InterruptedException {
+	public void reciever(String username, String password) throws InterruptedException {
 //		System.out.println("I'm reciever for login");
 		Reporter.log("I'm reciever for login");
+		Reporter.log("I'm reciever for login",true); // to see the message on cosole
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15000));
-		driver.get("https://demowebshop.tricentis.com/");
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 
-		driver.findElement(By.partialLinkText("Log in")).click();
-		driver.findElement(By.xpath("//input[@id='Email']")).sendKeys(username);
-		driver.findElement(By.xpath("//input[@id='Password']")).sendKeys(password);
-		driver.findElement(By.xpath("//input[@value='Log in']")).click();
+		driver.findElement(By.xpath("//input[@name='username']")).sendKeys(username);
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys(password);
+		driver.findElement(By.xpath("//button[text()=' Login ']")).click();
 
 		Thread.sleep(3000);
 //		WebElement isValidate = driver.findElement(By.partialLinkText("Log out"));
